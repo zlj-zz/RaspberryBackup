9472350e774a1934d685284b70f8751b77f10e5b
Zachary <zlj19971222@outlook.com>
2021-04-14 22:30:42 +0800

    chore: add some progress display, modify output.

--- rpi-backup.sh
+++ rpi-backup.sh
@@ -5,49 +5,52 @@ if [  $# != 2 ]; then
   echo "example: $0 /dev/mmcblk0p1 /dev/mmcblk0p2"
   exit 0
 fi
+
+echo "=====================   part 1, preparation    ==============================="
 dev_boot=$1
 dev_root=$2
 mounted_boot=`df -h | grep $dev_boot | awk '{print $6}'`
 mounted_root=`df -h | grep $dev_root | awk '{print $6}'`
 img=rpi-`date +%Y%m%d-%H%M`.img
 
-#install tools
+echo "install tools ..."
 sudo apt-get install dosfstools dump parted kpartx
 
-echo =====================   part 1, prepare workspace    ===============================
+echo "prepare workspace ..."
 mkdir ~/backupimg
 cd ~/backupimg
 
-echo ===================== part 2, create a new blank img ===============================
 # New img file
-#sudo rm $img
-
 bootsz=`df -P | grep $dev_boot | awk '{print $2}'`
 rootsz=`df -P | grep $dev_root | awk '{print $3}'`
 totalsz=`echo $bootsz $rootsz | awk '{print int(($1+$2)*1.3/1024)}'`
-sudo dd if=/dev/zero of=$img bs=1M count=$totalsz
+echo "created a blank img, size ${totalsz}M ..."
+sudo dd if=/dev/zero of=$img bs=1M count=$totalsz status=progress
 #sync
-echo "...created a blank img, size ${totalsz}M "
 
 # format virtual disk
 bootstart=`sudo fdisk -l | grep $dev_boot | awk '{print $2}'`
 if [ -eq $bootstart '*' ]; then
   bootstart=`sudo fdisk -l | grep $dev_boot | awk '{print $3}'`
   bootend=`sudo fdisk -l | grep $dev_boot | awk '{print $4}'`
+else
+  bootend=`sudo fdisk -l | grep $dev_boot | awk '{print $3}'`
 fi
-bootend=`sudo fdisk -l | grep $dev_boot | awk '{print $3}'`
 rootstart=`sudo fdisk -l | grep $dev_root | awk '{print $2}'`
-echo "boot: $bootstart >>> $bootend, root: $rootstart >>> end"
 #有些系统 sudo fdisk -l 时boot分区的boot标记会标记为*,此时bootstart和bootend最后应改为 $3 和 $4
 #rootend=`sudo fdisk -l /dev/mmcblk0 | grep mmcblk0p2 | awk '{print $3}'`
+echo "boot: $bootstart >>> $bootend, root: $rootstart >>> end; initialize backup img ..."
 
+# initialize backup img.
 sudo parted $img --script -- mklabel msdos
 sudo parted $img --script -- mkpart primary fat32 ${bootstart}s ${bootend}s
 sudo parted $img --script -- mkpart primary ext4 ${rootstart}s -1
 
-echo =====================  part 3, mount img to system  ===============================
+echo "=====================  part 2, mount img to system  ==============================="
 loopdevice=`sudo losetup -f --show $img`
+echo "losetup result: ${loopdevice}"
 device=/dev/mapper/`sudo kpartx -va $loopdevice | sed -E 's/.*(loop[0-9]+)p.*/\1/g' | head -1`
+echo "kpartx result: ${device}"
 sleep 5
 sudo mkfs.vfat ${device}p1 -n boot
 sudo mkfs.ext4 ${device}p2 -L rootfs
@@ -60,12 +63,12 @@ sudo mount -t vfat -o uid=${uid},gid=${gid},umask=0000 ${device}p1 ./tgt_boot/
 sudo mount -t ext4 ${device}p2 ./tgt_Root/
 
 
-echo ===================== part 4, backup /boot =========================
+echo "===================== part 3, backup /boot ========================="
 sudo cp -rfp ${mounted_boot}/* ./tgt_boot/
 sync
 echo "...Boot partition done"
 
-echo ===================== part 5, backup / =========================
+echo "===================== part 4, backup / ========================="
 sudo chmod 777 ./tgt_Root
 sudo chown ${uid}.${gid} tgt_Root
 sudo rm -rf ./tgt_Root/*
@@ -76,7 +79,7 @@ sync
 echo "...Root partition done"
 cd ..
 
-echo ===================== part 6, replace PARTUUID =========================
+echo "===================== part 5, replace PARTUUID ========================="
 
 # replace PARTUUID
 opartuuidb=`sudo blkid -o export $dev_boot | grep PARTUUID`
@@ -94,11 +97,13 @@ cd ~/backupimg/tgt_Root
 sudo rm -rf ./.gvfs ./dev/* ./media/* ./mnt/* ./proc/* ./run/* ./sys/* ./tmp/* ./lost+found/ ./restoresymtable
 cd ..
 
-echo ===================== part 7, unmount =========================
+echo "===================== part 6, unmount ========================="
 sudo umount tgt_boot tgt_Root
 sudo kpartx -d $loopdevice
 sudo losetup -d $loopdevice
 rmdir tgt_boot tgt_Root
+echo "...unmount done"
 
 
-echo "==== All done. img file is under ~/backupimg/ "
+echo "\nWhere the img?"
+echo "    img file is under ~/backupimg/ "
